a7c90cc31d033d2da798a07d86170bc5f6631693
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-04 12:08:29 +0300

    done

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * insert_node - fefef
+ * @head: fasf
+ * @number: fsfsd
+ * Return: fasfsa
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *h = *head, *prev = *head, *node = NULL;
+
+	node = malloc(sizeof(listint_t));
+	if (!node || !number)
+		return (NULL);
+
+	node->n = number;
+
+	if (!*head)
+	{
+		*head = node;
+		return (node);
+	}
+
+	if (h->n > number)
+	{
+		node->next = h;
+		h = node;
+		return (node);
+	}
+
+	while (h)
+	{
+		if (h->n > number)
+		{
+			node->next = h;
+			prev->next = node;
+			return (node);
+		}
+		prev = h;
+		h = h->next;
+	}
+	node->next = h;
+	prev->next = node;
+	return (node);
+}
