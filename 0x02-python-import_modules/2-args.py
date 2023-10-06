9e8cedd4ade4655dd2e5deb03127b4867a134918
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-06 03:22:44 +0300

    fas

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     argc = len(argv) - 1
-    ctr = 1
-    print(
-            "{} {}{}"
-            .format(argc, "argument" if argc == 1
-                    else "argumentsi", ':' if argc > 0 else '.')
-        )
-    if argc:
-        for arg in argv:
-            if arg != argv[0]:
-                print("{}: {}".format(ctr, arg))
-                ctr += 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, argv[i + 1]))
