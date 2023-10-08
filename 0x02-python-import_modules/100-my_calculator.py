e60419605640fc47cf1460618b279ed26ff66512
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-08 15:45:56 +0300

    fas

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,21 @@
 if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
-    print(sys.argv)
     if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        sys.exit(1)
-    args = sys.argv[1:]
-    if args[1] not in ('+', '-', '/', '*'):
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    res = "{} {} {} = {}"
+    if op == '+':
+        print(res.format(a, '+', b, add(a, b)))
+    elif op == '*':
+        print(res.format(a, '*', b, mul(a, b)))
+    elif op == '/':
+        print(res.format(a, '/', b, div(a, b)))
+    elif op == '-':
+        print(res.format(a, '-', b, sub(a, b)))
+    else:
         print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
-    a = int(args[0])
-    b = int(args[2])
-    res = "{:d} {:s} {:d} = {}"
-    match args[1]:
-        case '+':
-            print(res.format(a, args[1], b, add(a, b)))
-        case '-':
-            print(res.format(a, args[1], b, sub(a, b)))
-        case '/':
-            print(res.format(a, args[1], b, div(a, b)))
-        case '*':
-            print(res.format(a, args[1], b, mul(a, b)))
+        exit(1)
