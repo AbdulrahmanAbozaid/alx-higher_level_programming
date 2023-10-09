26d1dda70c1e87e7ca49991328a2d0bf37a6fde9
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-09 14:18:46 +0300

    fas

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    res = ()
-    res += (0 if len(tuple_a) < 1 else tuple_a[0]) +\
-        (0 if len(tuple_b) < 1 else tuple_a[0]),
-    res += (0 if len(tuple_a) < 2 else tuple_a[1]) +\
-        (0 if len(tuple_b) < 2 else tuple_a[1]),
-    return res
+    ta1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    ta2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    tb1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    tb2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (ta1 + tb1, ta2 + tb2)
