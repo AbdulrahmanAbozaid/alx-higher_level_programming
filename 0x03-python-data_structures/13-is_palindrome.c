8fc9e8ae10a993ba24a1ac635b48ff5e4c6fd57b
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-09 16:06:41 +0300

    fas

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * is_palindrome - fsaff
+ * @head: dasfas
+ * Return: fafsaf
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int *nums, len, i = 0;
+	listint_t *l = *head;
+
+	if (!head || !*head)
+		return (1);
+	while (l)
+	{
+		len++;
+		l = l->next;
+	}
+
+	nums = malloc(sizeof(int) * len);
+	l = *head;
+	while (i < len / 2)
+	{
+		*nums = l->n;
+		i++;
+		nums++;
+		l = l->next;
+	}
+
+	i--;
+	nums--;
+	if (len % 2 != 0)
+		l = l->next;
+
+	while (i >= 0)
+	{
+		if (l->n != *nums)
+		{
+			return (0);
+		}
+		l = l->next;
+		i--;
+		nums--;
+	}
+
+	return (1);
+}
