85e1573a6b521495de4cc6269cb819efc8658714
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-24 22:53:04 +0300

    fas

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+def safe_print_division(a, b):
+    try:
+        val = a / b
+    except Exception:
+        val = None
+    finally:
+        print("Inside result: ", end="")
+        print("{}".format(val))
+        return val
