a19a852dcfbad85a3f3580316d3036d3314cc099
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-26 01:59:24 +0300

    fas

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Module define squares"""
+
+
+class Square:
+    """Square Class to define squares """
+    def __init__(this, size):
+        """Initialize Square"""
+        this.__size = size
