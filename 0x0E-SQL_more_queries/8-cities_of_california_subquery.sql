86df92acb69aaec45dc98a14dc760a1f8dcd257b
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-03-14 02:32:42 +0200

    SQL1

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,5 @@
 -- calilinux
-SELECT c.id, c.name FROM states s, cities c WHERE s.id = c.state_id ORDER BY c.id;
+SELECT c.id, c.name
+FROM states s, cities c
+WHERE s.id = 'California'
+ORDER BY c.id ASC;
