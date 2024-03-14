4f875e7839ea8b060b3d4debdd3dcfe26595035d
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-03-14 03:34:30 +0200

    SQL0

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,2 @@
 -- NUmbe
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as "number_of_shows" FROM tv_genres INNER
- JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre ORDER BY COUNT(tv_show_genres.show_id) DESC;
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as "number_of_shows" FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY genre ORDER BY COUNT(tv_show_genres.show_id) DESC;
